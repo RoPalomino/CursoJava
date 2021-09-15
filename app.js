3f4f933f7c9a6c5178738782237da983006cc982
RoPalomino <89050696+RoPalomino@users.noreply.github.com>
2021-09-14 21:18:08 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let numero1 = Number(prompt('ingresa un numero'))
+let numero2 = Number(prompt('ingresa un numero'))
+
+console.log (numero1 + numero2)
+console.log (numero1 - numero2)
+console.log (numero1 * numero2)
+console.log (numero1 / numero2)
